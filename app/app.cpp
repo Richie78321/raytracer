3776d239b550082ef32a9c0e3b2caa8ab3e2488c
Richie Goulazian <rgoulazian@gmail.com>
2021-09-16 20:18:17 -0400

    Fix funky quaternion mult order for camera control

--- app/app.cpp
+++ app/app.cpp
@@ -15,19 +15,19 @@ float4 fromAxisAngle(float3 axis, float angle) {
 
 void cameraControl(SceneCamera& camera) {
     if (IsKeyPressed(KEY_LEFT)) {
-      camera.rotation = linalg::qmul(camera.rotation, fromAxisAngle(float3{ 0, 0, 1 }, (float)(5 * DEG2RAD)));
+      camera.rotation = linalg::qmul(fromAxisAngle(float3{ 0, 0, 1 }, (float)(-10 * DEG2RAD)), camera.rotation);
     }
 
     if (IsKeyPressed(KEY_RIGHT)) {
-      camera.rotation = linalg::qmul(camera.rotation, fromAxisAngle(float3{ 0, 0, 1 }, (float)(-5 * DEG2RAD)));
+      camera.rotation = linalg::qmul(fromAxisAngle(float3{ 0, 0, 1 }, (float)(10 * DEG2RAD)), camera.rotation);
     }
 
     if (IsKeyPressed(KEY_UP)) {
-      camera.rotation = linalg::qmul(camera.rotation, fromAxisAngle(float3{ 1, 0, 0 }, (float)(5 * DEG2RAD)));
+      camera.rotation = linalg::qmul(camera.rotation, fromAxisAngle(float3{ 1, 0, 0 }, (float)(10 * DEG2RAD)));
     }
 
     if (IsKeyPressed(KEY_DOWN)) {
-      camera.rotation = linalg::qmul(camera.rotation, fromAxisAngle(float3{ 1, 0, 0 }, (float)(-5 * DEG2RAD)));
+      camera.rotation = linalg::qmul(camera.rotation, fromAxisAngle(float3{ 1, 0, 0 }, (float)(-10 * DEG2RAD)));
     }
 
     if (IsKeyPressed(KEY_E)) {
